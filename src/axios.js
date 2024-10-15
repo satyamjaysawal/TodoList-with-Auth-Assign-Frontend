88c79c61dfe3e7f0bb5c9f2357aab3e397d1d5be
Satyam_jaysawal <sjrecm9258@gmail.com>
2024-10-15 19:22:36 +0530

    f

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: 'http://localhost:5000/api',
-    baseURL: process.env.REACT_APP_API_BASE_URL,
+    baseURL: 'http://localhost:5000/api',
+    // baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 export default instance;
