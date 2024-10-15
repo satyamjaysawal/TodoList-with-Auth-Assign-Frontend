a44896e7d10e87df664a2267f5216d5d5c9d8ef6
Satyam_jaysawal <sjrecm9258@gmail.com>
2024-10-15 21:44:58 +0530

    first

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const instance = axios.create({
     // baseURL: 'http://localhost:5000/api',
     // baseURL: process.env.REACT_APP_API_BASE_URL,
-    baseURL: 'https://todolist-with-auth-assign-backend.onrender.com/api'
+    // baseURL: 'https://todolist-with-auth-assign-backend.onrender.com/api'
+    baseURL: 'https://ttm-sable.vercel.app/api'
 });
 
 export default instance;
