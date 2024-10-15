6c347b624e0e0476ae00963e75b5bed1e87b2847
Satyam_jaysawal <sjrecm9258@gmail.com>
2024-10-15 19:29:53 +0530

    f

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    // baseURL: 'http://localhost:5000/api',
     // baseURL: process.env.REACT_APP_API_BASE_URL,
+    baseURL: 'https://todolist-with-auth-assign-backend.onrender.com/api'
 });
 
 export default instance;
